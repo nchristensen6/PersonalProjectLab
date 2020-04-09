9e17390485a475b9f2984f2d92946aac03f8ba0f
nchristensen6 <59981868+nchristensen6@users.noreply.github.com>
2020-04-08 21:10:50 -0500

    Finishing the project

--- PersonalProjectLab/PersonalProjectLab/EmptyClass.cs
+++ PersonalProjectLab/PersonalProjectLab/EmptyClass.cs
@@ -3,8 +3,47 @@ namespace PersonalProjectLab
 {
     public class EmptyClass
     {
-        public EmptyClass()
+        public static void GetPitch()
         {
+            Console.WriteLine("Please input the number that you are currently at in the batting order from 1 to 9");
+            string BatterNumber = Console.ReadLine();
+            int BattingOrder = Convert.ToInt32(BatterNumber);
+            if (BattingOrder == 1)
+            {
+                Console.WriteLine("Throw a Fastball.");
+            }
+            if (BattingOrder == 2)
+            {
+                Console.WriteLine("Throw a Curveball");
+            }
+            if (BattingOrder == 3)
+            {
+                Console.WriteLine("Throw a Slider");
+            }
+            if (BattingOrder == 4)
+            {
+                Console.WriteLine("Throw a Sinker");
+            }
+            if (BattingOrder == 5)
+            {
+                Console.WriteLine(" Throw a Screwball");
+            }
+            if (BattingOrder == 6)
+            {
+                Console.WriteLine(" Throw a Changeup");
+            }
+            if (BattingOrder == 7)
+            {
+                Console.WriteLine("Throw a Forkball");
+            }
+            if (BattingOrder == 8)
+            {
+                Console.WriteLine("Throw a Splitter");
+            }
+            if (BattingOrder == 9)
+            {
+                Console.WriteLine("Throw a Knuckleball");
+            }
         }
     }
 }
--- PersonalProjectLab/PersonalProjectLab/Program.cs
+++ PersonalProjectLab/PersonalProjectLab/Program.cs
@@ -6,7 +6,7 @@ namespace PersonalProjectLab
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            EmptyClass.GetPitch();
         }
     }
 }
